cb3110cfb865ac6c7b22d65e4fd6da9eb98e0045
valentinacozzi <valentina.cozzi@studio.unibo.it>
2022-06-29 19:06:12 +0200

    Update story_js.js

--- assets/js/story_js.js
+++ assets/js/story_js.js
@@ -19,3 +19,50 @@ listItems.forEach(item =>{
     observer.observe(item);
 
 })
+
+
+
+
+// ----------- FADE IN TEXT SECTION -------------
+
+$(window).on('scroll.scroll6',function() {
+    if (checkVisible($('.double-text-section1'))) { 
+		$('.text-section-below').delay(1000).fadeIn(2000);
+		$('.text-section-below').addClass( "entrance-down" );
+        $(window).off('scroll.scroll6');
+    } else {
+         // do nothing
+    }
+});
+
+$(window).on('scroll.scroll8',function() {
+    if (checkVisible($('.text-section-start'))) { 
+		$('.bottom-paragraph').delay(2000).fadeIn(2000);
+		$('.small-arrow3').delay(4000).fadeTo(2000,1);
+        $(window).off('scroll.scroll8');
+    } else {
+         // do nothing
+    }
+});
+
+$(window).on('scroll.scroll18',function() {
+    if (checkVisible($('.text-section-initial'))) { 
+		$('.bottom-paragraph2').delay(1500).fadeIn(2000);
+		$('.bottom-paragraph2').addClass( "entrance-down" );
+		$('.second-arrow2').delay(3000).fadeTo(2000,1);
+        $(window).off('scroll.scroll18');
+    } else {
+         // do nothing
+    }
+});
+
+$(window).on('scroll.scroll23',function() {
+    if (checkVisible($('.text-section-final-two'))) { 
+		$('.bottom-paragraph3').delay(1500).fadeIn(2000);
+		$('.bottom-paragraph3').addClass( "entrance-down" );
+		$('.second-arrow').delay(4000).fadeTo(2000,1);
+        $(window).off('scroll.scroll23');
+    } else {
+         // do nothing
+    }
+});
